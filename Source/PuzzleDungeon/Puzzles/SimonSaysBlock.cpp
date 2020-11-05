9fa444bdaae9e37df5ba93187dd2a0d702615a47
Dejavu1 <jontyhoekstra@gmail.com>
2020-11-05 14:47:59 +1300

    I finished simon says

---
+++ Source/PuzzleDungeon/Puzzles/SimonSaysBlock.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SimonSaysBlock.h"
+
+// Sets default values
+ASimonSaysBlock::ASimonSaysBlock()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASimonSaysBlock::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASimonSaysBlock::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
